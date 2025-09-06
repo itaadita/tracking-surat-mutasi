295aec82e5ef3bda9b98ae0b8df5ed38c64d88b2
itaadita <ditarsip@gmail.com>
2025-09-06 18:25:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ sheet = spreadsheet.get_worksheet(0)
 
 # --- Refresh data ---
 if 'df' not in st.session_state or st.button("🔄 Refresh Data"):
-    data = sheet.get_all_records() 
+    data = sheet.get_all_records(head=10) 
     st.session_state.df = pd.DataFrame(data)
     st.session_state.last_refresh = pd.Timestamp.now()
 
@@ -160,6 +160,7 @@ if nip:
 
     else:
         st.warning("NIP tidak ditemukan.")
+
 
 
 
